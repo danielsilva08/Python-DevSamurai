c78124aa93ef44443fc2f92388c1e5e9935c594c
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 16:55:34 -0300

    Create dic5.py

---
+++ dic5.py
@@ -0,0 +1,17 @@
+#DICIONÁRIOS!!!
+#listas usamos[]
+#tuplas usamos()
+#strings usamos '' ou ""
+#dicionários {chave: valor}-> chave poderá ser uma string ou um int
+#O valor pode ser lista, string.
+
+#criando um dicionário
+empregados = {'01':['João',' Silva'], 
+            '02':['Maria','das Graças'],
+            '03':['Paulo','José']}
+
+empregados2 = {'04':['Fulano','de Tal'],
+               '05':['Beltrano','de Tal']}
+#update concatena ou seja adiciona os dois empregados. não pode ter duas chaves ids iguais pois ele nã sobre escresve.
+empregados.update(empregados2)
+print(empregados)
