85e4f2e798f63d7869e57157e53f46f25b8ebd67
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 14:38:08 -0300

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,16 @@
+#TUPLAS!!
+
+t = 1234,3541,'hello'
+
+u = t, (1,2,3)
+
+x,y,z = t
+print(x,y,z)
+
+tupla_vazia = ()
+tupla_com_um_elemento = 'texto', #sem a virgula torna uma string
+print(type(tupla_com_um_elemento))
+
+a = t.count(1234)
+print(a)
+#Tuplas são imutaveis
