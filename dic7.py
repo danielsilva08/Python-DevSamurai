143a2a084928913a97be8cd4a3c6f61853507edd
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 17:06:48 -0300

    Create dic7.py

---
+++ dic7.py
@@ -0,0 +1,7 @@
+#Medotos úteis em dicionários são: keys(), value(), itens()
+dias = {'Sex':'sexta','Seg':'segunda','Qua':'Quarta','Qui':'Quinta'}
+
+values = dias.values()
+#print(chaves)
+for value in values: #Para cada valor que esta dentro das linhas valores imprima valores
+    print(value, end=' ')#' 'para dar espaço entre os dis da semana
