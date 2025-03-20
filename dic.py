cf0ee7fcb53e4234ed30ada0b6855afda4cee221
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 14:56:00 -0300

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,13 @@
+#DICIONÁRIOS!!!
+#listas usamos[]
+#tuplas usamos()
+#strings usamos '' ou ""
+#dicionários {chave: valor}-> chave poderá ser uma string ou um int
+#O valor pode ser lista, string.
+
+#criando um dicionário
+empregados = {'01':['João',' Silva'], 
+            '02':['Maria','das Graças'],
+            '03':['Paulo','José']}
+#print(type(empregados))
+print(empregados['01'])#acessa valores atraves das chaves
