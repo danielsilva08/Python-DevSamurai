7fd0a690cd341fa2f496bd963bcdca73f99f1f3a
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 17:03:47 -0300

    Create dic6.py

---
+++ dic6.py
@@ -0,0 +1,7 @@
+#Medotos úteis em dicionários são: keys(), value(), itens()
+dias = {'Sex':'sexta','Seg':'segunda','Qua':'Quarta','Qui':'Quinta'}
+
+chaves = dias.keys()
+#print(chaves)
+for chave in chaves: #Para cada chave que esta dentro das linhas chaves imprima chave
+    print(chave)
