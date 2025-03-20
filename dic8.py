a6e242c6660f31b36db0e547abf5501315bd2724
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 17:10:02 -0300

    Create dic8.py

---
+++ dic8.py
@@ -0,0 +1,11 @@
+#Medotos úteis em dicionários são: keys(), value(), itens()
+dias = {'Sex':'sexta','Seg':'segunda','Qua':'Quarta','Qui':'Quinta'}
+
+values = dias.values()
+
+for item in dias.items(): 
+    print(item)
+    #imprimi as chaves e valores
+#dias.pop('sex')    
+#print(dias)
+#pop remove a chave que eu quiser
