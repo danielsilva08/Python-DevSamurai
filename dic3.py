e8543548a9e1adcd105d6e72fc74c6b91a4a1f4a
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-20 16:03:22 -0300

    Create dic3.py

---
+++ dic3.py
@@ -0,0 +1,15 @@
+#DICIONÁRIOS!!!
+#listas usamos[]
+#tuplas usamos()
+#strings usamos '' ou ""
+#dicionários {chave: valor}-> chave poderá ser uma string ou um int
+#O valor pode ser lista, string.
+
+#criando um dicionário
+empregados = {'01':['João',' Silva'], 
+            '02':['Maria','das Graças'],
+            '03':['Paulo','José']}
+
+numero_func = input('Digite o numero do funcionario:\n')
+nome = input('Digite o nome:\n')
+     
