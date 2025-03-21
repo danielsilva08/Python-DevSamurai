eedfecab971d4f149eeaf32f24bf602033533817
Daniel Silva <daniel_silva008@hotmail.com>
2025-03-21 15:32:48 -0300

    Create POO.py

---
+++ POO.py
@@ -0,0 +1,17 @@
+#Programação Orientada a Objetos POO
+#Python permite programar tanto em POO como em estrutura
+
+#Objetos vão métodos e atributos
+
+#classe é um projeto de um objeto
+class Pessoa:
+    pass
+
+pessoa1 = Pessoa()
+pessoa2 = Pessoa()
+
+pessoa1.nome = 'Maria'
+pessoa2.nome = 'João'
+
+pessoa1.idade = 30
+print(pessoa1.idade)
